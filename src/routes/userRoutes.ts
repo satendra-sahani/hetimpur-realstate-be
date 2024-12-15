74a6a9ac8e0e0aaa284cd56d77f53a7575b92184
Satendra Sahani <satendrasahani270@gmail.com>
2024-12-15 15:14:42 +0530

    updated code

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { generatePaymentLink } from '../controllers/paymentController';
 import { authMiddleware } from '../middleware/auth';
+import { getLands } from '../controllers/clientController';
 const router = express.Router();
 
 router.post('/razorpay/generate-payment-link',authMiddleware(["user","client"]), generatePaymentLink);
-
+router.get('/lands', getLands);
 export default router;
